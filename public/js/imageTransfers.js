961101b7fbdb93aec8fa77cc6742f5762020a20d
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2015-12-01 12:31:19 -0800

    Begin file transfer

--- public/js/imageTransfers.js
+++ public/js/imageTransfers.js
@@ -10,3 +10,14 @@ var saveToFirebase = function(){
 
   console.log(authData)
 };
+
+var refPics = new Firebase('https://dazzling-fire-2339.firebaseio.com/pics');
+// Attach an asynchronous callback to read the data at our posts reference
+var retFromFirebase = function(){
+  refPics.on("value", function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+};
