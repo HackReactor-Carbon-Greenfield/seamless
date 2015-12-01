2f98ddc9ad4f4e72845871495e3964e86a2c1186
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2015-11-30 16:18:34 -0800

    Add authentication file

---
+++ public/js/auth.js
@@ -0,0 +1,14 @@
+var ref = new Firebase("https://dazzling-fire-2339.firebaseio.com");
+
+
+var login = function(){
+
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+
+}
