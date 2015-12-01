233f6c9b00d9f153e1a0c1177f5ba91ef1e2f3e8
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2015-11-30 16:38:48 -0800

    Add logout functionality

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,13 +2,16 @@ var ref = new Firebase("https://dazzling-fire-2339.firebaseio.com");
 
 
 var login = function(){
+  ref.authWithOAuthPopup("google", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+};
 
-ref.authWithOAuthPopup("google", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
-
-}
+var logout = function(){
+  ref.unauth();
+  console.log('logged out')
+};
